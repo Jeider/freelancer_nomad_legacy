8878fc5ec56398e6b46572bbc0d058cadef892ec
Jeider <flmatrix@yandex.ru>
2019-04-20 00:05:51 +0300

    Missions, Randommissions, Scripts

---
+++ DATA/SCRIPTS/PL04/pl_04_dig_ambi_int_01.thn.lua
@@ -0,0 +1,47 @@
+duration=450
+
+entities={
+	{
+		up=1,
+		entity_name="Layer_pl_04_dig_ambi_int",
+		usr_flg=0,
+		ambient={
+			7,
+			7,
+			101
+		},
+		template_name="",
+		spatialprops={
+			orient={
+				{
+					1,
+					0,
+					0
+				},
+				{
+					0,
+					1,
+					0
+				},
+				{
+					0,
+					0,
+					1
+				}
+			},
+			pos={
+				0,
+				0,
+				0
+			}
+		},
+		front=2,
+		type=9,
+		srt_grp=0,
+		lt_grp=0
+	}
+}
+
+events={}
+
+
