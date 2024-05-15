82271f95bac9ba5ce99fbe53a3bb75c47dcdcc6a
Yuriy Filatov <flmatrix@yandex.ru>
2024-05-15 22:33:01 +0300

    Sync audio changes

---
+++ Assets/Pythonlancer/tools/audio.py
@@ -0,0 +1,16 @@
+
+
+txt = '''
+XMLUTF.exe -O echo_m01_mod.utf echo_m01.xml/echo_m01.utf.xml
+XMLUTF.exe -O echo_m01_player.utf echo_m01_player.xml/echo_m01_player.utf.xml
+
+XMLUTF.exe -O echo_m02_mod.utf echo_m02.xml/echo_m02.utf.xml
+XMLUTF.exe -O echo_m02_player.utf echo_m02_player.xml/echo_m02_player.utf.xml
+
+
+XMLUTF.exe -O echo_m08_mod.utf echo_m08.xml/echo_m08.utf.xml
+XMLUTF.exe -O echo_m08_female.utf echo_m08_female.xml/echo_m08.utf.xml
+XMLUTF.exe -O echo_m08_player.utf echo_m08_player.xml/echo_m08.utf.xml
+
+
+'''
