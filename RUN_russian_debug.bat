6566222f9a2431316ba830ed4554420cbd0707b7
Yuriy Filatov <flmatrix@yandex.ru>
2025-10-26 00:47:15 +0300

    Improve dbg

--- RUN_russian_debug.bat
+++ RUN_russian_debug.bat
@@ -1,2 +1,3 @@
 powershell -command "Unblock-File -Path (\"{0}{1}\" -f ($pwd).path, \"\python\Lib\site-packages\pythonnet\runtime\Python.Runtime.dll\")"
 .\python\python.exe .\install.py ru debug
+timeout 5 > NUL
