d3df481da19ca76e5b89b0e8aa8297f538d1ef7c
Yuriy Filatov <flmatrix@yandex.ru>
2026-08-11 23:18:26 +0300

    Ideas

---
+++ Assets/Pythonlancer/ideas.py
@@ -0,0 +1,91 @@
+arch = '''
+1 - rubic
+2 - rubic
+3 - rubic + shield battle
+
+'''
+
+megacannon = '''
+1 - outposts / solar plants
+2 - outposts / solar plants
+3 - megacannon
+'''
+
+
+
+
+missions = '''
+
+story
+
+arch
+megacannon
+
+alien
+
+lair
+sphere
+monkey
+anomaly - purple
+ice
+red giant
+neutron star
+black hole
+
+rheinland
+
+capital
+military-assault-smuggler
+- attack correct freighter
+- attack correct smuggler outpost
+- destroy pirate bases
+military-defend
+- defend liner by destroy correct fighter
+- attack correct pirate base (outpost)
+- destroy pirate battleship
+pirate-miner
+- attack roid miner 1-2
+- attack mannhaim - destroy trade containers
+pirate-tlr
+- attack tlr 1-2
+- destroy correct trade base
+
+
+smuggler types:
+- default smuggler
+- production
+- gas mining
+
+trading types:
+- trading outpost
+
+station types:
+- outpost
+- shieldgen outpost
+
+
+kusari blood dragon mission
+- destroy kusari outpost by destroy correct fighter
+- attack planet - destroy correct kusari legal cruiser
+- join fight with order - destroy order correct fighter
+--- fly "back"!
+[step2]
+- return back to planet and power up shields
+- attack shinja - destoy shinja correct gunboat
+- attack shinja battleships
+--- fly "back"!
+[step3]
+- attack order battleship
+
+mission story ideas
+- enter by reputation (cutscene playback)
+- play it
+- destroy final object to increase rep
+- go next mission
+
+
+
+
+
+
+'''
